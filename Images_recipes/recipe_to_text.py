5bb0e983643e0e54846ed1a181260a8fb7920c50
varunharbola <56774390+varunharbola@users.noreply.github.com>
2020-02-15 12:52:15 -0800

    Update recipe_to_text.py

--- Images_recipes/recipe_to_text.py
+++ Images_recipes/recipe_to_text.py
@@ -13,6 +13,8 @@ import pyttsx3
 engine = pyttsx3.init()
 
 
+
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\varun\Anaconda3\Lib\site-packages\pytesseract\tesseract'
 x=pytesseract.image_to_string(Image.open('rattatouille_instructions.png'))
 #x=''.join(z for z in x if x.isalnum())
@@ -20,9 +22,20 @@ x=' '.join(x.split())
 x=re.split('[0-9]*\.',x)
 #x=(x.split(','))
 print(x[5])
+i=0
 for z in x:
-    
+    number_i=str(i)
+    '''
+    if z=='INSTRUCTIONS':
+        i=i+1
+    else:
+    '''
     engine.say(z)
-engine.runAndWait()
+    
+    #input("Press Enter to continue...")
+    
+    engine.runAndWait()
+    print(i)
+    time.sleep(2)
 
 
