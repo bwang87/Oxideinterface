4b4d285cb0ef0f2df0bc57aef7abab564feb5e01
Prastuti Singh <singhprastuti@gmail.com>
2020-02-15 17:58:03 -0800

    read recipe updated

--- read_recipe.py
+++ read_recipe.py
@@ -1,25 +1,32 @@
-"""Detects text in the file."""
 from google.cloud import vision
 import io
 import os
-import re
-import numpy as np
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'Recipe-Bot-e5867579a186.json'
 client = vision.ImageAnnotatorClient()
 
-def tokenizer(filename):
+def tokenizer(recipe_folder_name):
+    cwd = os.getcwd()
+    recipes_folder = 'Images_recipes'
+
     # Process image and extract ingredient list and instructions list
-    with io.open(filename, 'rb') as image_file:
-        content = image_file.read()
+    filepath = os.path.join(cwd, recipes_folder, recipe_folder_name)
 
-    image = vision.types.Image(content=content)
+    all_files = []
 
-    response = client.text_detection(image=image)
+    for filename in sorted(os.listdir(filepath)):
+        with io.open(os.path.join(filepath, filename), 'rb') as image_file:
+            content = image_file.read()
+            image = vision.types.Image(content=content)
+            response = client.text_detection(image=image)
+            text = response.full_text_annotation.text
+            text = text.replace('•', '')
+            text = text.split('\n')
+            all_files.append(text)
 
-    text = response.full_text_annotation.text
-    text = text.replace('•', '')
-    all_text = text.split('\n')
+    all_text = []
+    for list in all_files:
+        all_text = all_text + list
 
     ingredient_index = 0
     instructions_index = 0
